d59ff38dca6428e027b8027114e2e46c4297e868
eddy49reg <akhnazarov1990@gmail.com>
2023-01-19 18:21:38 +0300

    fill the app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+require('@babel/register');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const ssr = require('./middleware/ssr');
+
+const app = express();
+
+const PORT = 3000;
+// const usersRoute = require('./routes/users.route');
+// const carsRoute = require('./routes/cars.route');
+// const mainRoute = require('./routes/main.route');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(ssr);
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/', mainRoute);
+// app.use('/users', usersRoute);
+// app.use('/cars', carsRoute);
+
+app.listen(PORT, () => {
+  console.log(Сервер запущу на ${PORT}щу);
+});
