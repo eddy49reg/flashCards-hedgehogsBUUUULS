183e95a913d7b30309c19b774b1f6e0af3f9ce3f
PenRomain <romandreevich@icloud.com>
2023-01-19 19:08:14 +0300

    layout

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -7,9 +7,9 @@ module.exports = function Layout({ title, children }) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
+        <title>{title}</title>
       </head>
-      <body></body>
+      <body>{children}</body>
     </html>
   );
 };
